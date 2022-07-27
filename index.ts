c434f2158ea4a86e371ea107e507ee2ac69dda30
Adam Tate <99485792+at-vpd@users.noreply.github.com>
2022-07-27 10:55:28 +0100

    Yarn

--- index.ts
+++ index.ts
@@ -1,20 +1,20 @@
-import { DataSource, DeepPartial } from 'typeorm';
-import { Author } from './entities/Author';
-import { Category } from './entities/Category';
-import { Post } from './entities/Post';
+import { DataSource, DeepPartial } from "typeorm";
+import { Author } from "./entities/Author";
+import { Category } from "./entities/Category";
+import { Post } from "./entities/Post";
 
-import 'reflect-metadata';
-import { Request, Response } from 'express';
-import * as express from 'express';
-import * as bodyParser from 'body-parser';
-import { AppRoutes } from './routes';
+import "reflect-metadata";
+import { Request, Response } from "express";
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import { AppRoutes } from "./routes";
 
-export const AppDataSource = new DataSource({ type: 'sqljs' });
+export const AppDataSource = new DataSource({ type: "sqljs" });
 
-const author: DeepPartial<Author> = { name: 'Author1' };
-const category: DeepPartial<Category> = { name: 'Category1' };
+const author: DeepPartial<Author> = { name: "Author1" };
+const category: DeepPartial<Category> = { name: "Category1" };
 const posts: DeepPartial<Post>[] = [
-  { text: 'testTest', title: 'testTitle', author, categories: [category] },
+  { text: "testTest", title: "testTitle", author, categories: [category] }
 ];
 
 AppDataSource.initialize().then(async () => {
@@ -38,6 +38,6 @@ AppDataSource.initialize().then(async () => {
 
   // run app
   app.listen(3010, () =>
-    console.log('Express application is up and running on port 3000')
+    console.log("Express application is up and running on port 3000")
   );
 });
